1fb995abf65d8f1ebe8b453d56c33e70ef5f9b24
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-03-26 21:59:12 +0100

    Add: add correct delta of expenses compared to previous 30 days

--- src/app.py
+++ src/app.py
@@ -16,7 +16,6 @@ import datetime
 today = datetime.date.today()
 # get the last 30 days starting from today
 past = today - datetime.timedelta(days=30)
-previous_30_days = past - datetime.timedelta(days=30)
 
 # set the page default setting to wide
 st.set_page_config(layout="wide")
@@ -95,13 +94,13 @@ with st.sidebar:
 df_expenses.sort_values(by=["date"], inplace=True)
 print(df_expenses.head())
 
+previous_30_days = past_date - datetime.timedelta(days=30)
 # Filter data between the past date and 30 days earlier.
 # Useful to get the DELTA underneath the amount spent during the current timeframe
 df_expenses_previous_30_days = df_expenses.loc[
     (df_expenses["date"].dt.date >= previous_30_days)
-    & (df_expenses["date"].dt.date < past)
+    & (df_expenses["date"].dt.date < past_date)
 ]
-
 
 #  Filter data between two dates
 df_expenses_filtered = df_expenses.loc[
@@ -146,14 +145,20 @@ fig_bar_chart_months = px.bar(
 )
 
 # --- Metrics --- #
-
+# calculate total amount spent in the current timeframe selected
+current_total_expenses = round(df_expenses_filtered["value"].sum(), 2)
+# calculate the total amount spent in the previous 30 days from past_date
 total_expenses_previous_30_days = round(df_expenses_previous_30_days["value"].sum(), 2)
 
-# total expenses --- #
+# take the difference between the current timeframe and the previous 30 days, then show the value
+# as a metric
+diff_total_expenses = round(current_total_expenses - total_expenses_previous_30_days, 2)
+
+# --- Metric that shows the total amount spent in the previous 30 days from past_date --- #
 st.metric(
     label="Total amount spent in the timeframe selected",
-    value=round(df_expenses_filtered["value"].sum(), 2),
-    delta=total_expenses_previous_30_days,
+    value=current_total_expenses,
+    delta=diff_total_expenses,
 )
 
 # --- Create columns to position the plots --- #
@@ -171,8 +176,55 @@ plot3.plotly_chart(
 st.plotly_chart(
     fig_line_chart, use_container_width=True, sharing="streamlit", theme="streamlit"
 )
+
 st.plotly_chart(
     fig_bar_chart_months,
+    use_container_width=True,
+    sharing="streamlit",
+    theme="streamlit",
+)
+
+option = st.selectbox(
+    "Select the store",
+    df_expenses_filtered_year.sort_values(by="store", ascending=True)["store"]
+    .str.lower()
+    .unique(),
+    placeholder="Select the store...",
+)
+
+df_expenses_filtered_year_bar_plot = df_expenses_filtered_year.loc[
+    (df_expenses_filtered_year["store"] == option)
+]
+
+# --- Dropdown menu to select the store for the bar plot --- #
+
+# DOES NOT WORK WITH THE FILTERING! --> https://discuss.streamlit.io/t/filter-dataframe-by-selections-made-in-select-box/6627
+# get statistics per months, using a bar plot
+fig_bar_chart_week = px.bar(
+    df_expenses_filtered_year_bar_plot.groupby(["weekday_text"])["value"]
+    .agg(["mean"])
+    .reset_index(),
+    # df_expenses,
+    x="weekday_text",
+    y="mean",
+    # color="expense_category",
+    title="Expenses per weekday",
+)
+fig_bar_chart_week.update_xaxes(
+    categoryorder="array",
+    categoryarray=[
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ],
+)
+
+st.plotly_chart(
+    fig_bar_chart_week,
     use_container_width=True,
     sharing="streamlit",
     theme="streamlit",
@@ -187,3 +239,18 @@ st.plotly_chart(
 # in the metric!
 # You can calculate the previous month expense value compare to the current month
 # and then add a DELTA value in the metric to see if it went up or down.
+
+# TODO:
+# Add metric that computes the difference between the current timeframe
+# selected for total expenses and prior 30 days
+
+# TODO:
+# Add a line plot that plots the average spending for the week!
+
+# TODO:
+# in the plot for the weekday and the stores, groupby and show only the first 10 stores
+# with the highest values!
+
+# TODO:
+# add a metric to show the most expensive category of the timeframe selected
+# and the amount spent in the timeframe selected compared to the previous month
