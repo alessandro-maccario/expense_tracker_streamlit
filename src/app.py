dd84d256f79c17d9b170129357844d85958c3751
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-04-07 11:02:58 +0200

    Fix: move the dropdown menu for choosing the year right before the barplot that uses the filter

--- src/app.py
+++ src/app.py
@@ -27,10 +27,6 @@ with st.sidebar:
 
     # add sidebar title
     st.sidebar.title("Expense Tracker")
-    # st.sidebar.subheader(
-    #     """TEST EXPENSE TRACKER
-    # """
-    # )
 
     # choose the excel file to load
     uploaded_file = st.file_uploader("Choose a file")
@@ -60,15 +56,6 @@ with st.sidebar:
         # separator
         st.divider()
 
-        # select the year to filter out
-        choose_year = st.selectbox(
-            "Bar plot - Expenses per month",
-            df_expenses["year"].unique(),
-        )
-
-        # separator
-        st.divider()
-
     else:
         # load the expenses file
         df_expenses = pd.read_excel(
@@ -82,15 +69,6 @@ with st.sidebar:
         # define start and end date
         past_date = date1.date_input("From", past)
         today_date = date3.date_input("To", today)
-
-        # separator
-        st.divider()
-
-        # select the year to filter out
-        choose_year = st.selectbox(
-            "Select the year to filter the Bar Plot for the expense for month and category",
-            df_expenses["year"].unique(),
-        )
 
         # separator
         st.divider()
@@ -113,9 +91,6 @@ df_expenses_filtered = df_expenses.loc[
     (df_expenses["date"].dt.date >= past_date)
     & (df_expenses["date"].dt.date < today_date)
 ]
-
-#  Filter data for year
-df_expenses_filtered_year = df_expenses.loc[(df_expenses["year"] == int(choose_year))]
 
 # set the index using the expense_category column
 df_expenses_filtered_for_category = df_expenses_filtered.set_index("expense_category")
@@ -174,78 +149,7 @@ fig_pie_plot = px.pie(
 fig_pie_plot.update_traces(textposition="inside")
 fig_pie_plot.update_layout(uniformtext_minsize=12, uniformtext_mode="hide")
 
-
-# get statistics using a line chart
-fig_line_chart = px.line(
-    df_expenses.groupby("date").sum().reset_index(),
-    x="date",
-    y="value",
-    # color="year",
-    title="Expenses per month",
-)
-# Calculate monthly averages
-# Resample to month and calculate average value for each month
-df_expenses_subdf = df_expenses[["date", "value"]]
-df_expenses_subdf["value"] = pd.to_numeric(df_expenses_subdf["value"])
-# take the average daily expenses per month
-monthly_avg_values_per_day = df_expenses_subdf.resample("ME", on="date").sum() / 30
-# print(monthly_avg_values_per_day)
-
-# add horizontal line as the average line
-# fig_line_chart.add_hline(
-#     y=df_expenses.groupby("date").sum().reset_index()["value"].mean(),
-#     line_color="red",
-#     annotation_text="average",
-# )
-
-# Plot the average expenses per day in a month
-fig_line_chart.add_trace(
-    go.Scatter(
-        x=monthly_avg_values_per_day.index,
-        y=monthly_avg_values_per_day["value"],
-        mode="lines",
-        name="Daily Average per month",
-        line=dict(color="#FF0000"),
-    )
-)
-# Update layout
-fig_line_chart.update_layout(
-    title="Expenses Over Time", xaxis_title="Date", yaxis_title="Expenses"
-)
-
-
-# filter the df out using the "choose_year" filter for the year
-df_expenses_subdf_by_month_per_year = df_expenses[df_expenses["year"] == choose_year]
-# get only the month and the values
-df_expenses_subdf_by_month = df_expenses_subdf_by_month_per_year[["month", "value"]]
-# take the average daily expenses per month
-monthly_sum_values = df_expenses_subdf_by_month.groupby(["month"]).sum()
-# print(monthly_sum_values)
-# get statistics per months, using a bar plot
-fig_bar_chart_months = px.bar(
-    df_expenses_filtered_year.groupby(["expense_category", "month"])["value"]
-    .sum()
-    .reset_index(),
-    x="month",
-    y="value",
-    color="expense_category",
-    # title="Expenses per month and category",
-)
-# Plot the average expenses per month
-fig_bar_chart_months.add_trace(
-    go.Scatter(
-        x=monthly_sum_values.index,
-        y=monthly_sum_values["value"],
-        mode="lines",
-        name="Sum per month",
-        line=dict(color="#FF0000"),
-    )
-)
-# Update layout
-fig_bar_chart_months.update_layout(
-    title="Expenses per Month", xaxis_title="Date", yaxis_title="Expenses"
-)
-
+###################################################
 # --- Metrics --- #
 # calculate total amount spent in the current timeframe selected
 current_total_expenses = round(df_expenses_filtered["value"].sum(), 2)
@@ -279,7 +183,7 @@ diff_total_expenses_food = round(
 
 ##################################################
 # --- Create columns to position the metrics --- #
-metric1, metric2 = st.columns((2, 2))
+metric1, metric2, metric3, metric4 = st.columns((1, 1, 1, 1))
 
 # --- Metric that shows the total amount spent in the previous 30 days from past_date --- #
 metric1.metric(
@@ -314,8 +218,59 @@ plot3.plotly_chart(
     use_container_width=True,
 )
 
-st.plotly_chart(
-    fig_line_chart, use_container_width=True, sharing="streamlit", theme="streamlit"
+# separator
+st.divider()
+
+
+########################################################
+# --- Create columns to position the selection box --- #
+(
+    selectionbox_barplot1,
+    selectionbox_barplot2,
+    selectionbox_barplot3,
+    selectionbox_barplot4,
+) = st.columns((0.5, 1, 1, 1))
+
+# select the year to filter out
+choose_year = selectionbox_barplot1.selectbox(
+    "Choose the year",
+    df_expenses["year"].unique(),
+)
+
+########################################################
+
+#  Filter data for year
+df_expenses_filtered_year = df_expenses.loc[(df_expenses["year"] == int(choose_year))]
+# filter the df out using the "choose_year" filter for the year
+df_expenses_subdf_by_month_per_year = df_expenses[df_expenses["year"] == choose_year]
+# get only the month and the values
+df_expenses_subdf_by_month = df_expenses_subdf_by_month_per_year[["month", "value"]]
+# take the average daily expenses per month
+monthly_sum_values = df_expenses_subdf_by_month.groupby(["month"]).sum()
+
+# get statistics per months, using a bar plot
+fig_bar_chart_months = px.bar(
+    df_expenses_filtered_year.groupby(["expense_category", "month"])["value"]
+    .sum()
+    .reset_index(),
+    x="month",
+    y="value",
+    color="expense_category",
+    # title="Expenses per month and category",
+)
+# Plot the average expenses per month
+fig_bar_chart_months.add_trace(
+    go.Scatter(
+        x=monthly_sum_values.index,
+        y=monthly_sum_values["value"],
+        mode="lines",
+        name="Sum per month",
+        line=dict(color="#FF0000"),
+    )
+)
+# Update layout
+fig_bar_chart_months.update_layout(
+    title="Expenses per Month", xaxis_title="Months", yaxis_title="Expenses"
 )
 
 st.plotly_chart(
@@ -353,8 +308,15 @@ df_expenses_size_filtering_year = df_expenses_filtered_year.merge(
     how="inner",
 )
 
+# separator
+st.divider()
+
+########################################################
+# --- Create columns to position the selection box --- #
+selectionbox1, selectionbox2, selectionbox3, selectionbox4 = st.columns((0.8, 1, 1, 1))
+
 # show the store options
-option = st.selectbox(
+option = selectionbox1.selectbox(
     "Select the store",
     # show me only those options related to food
     df_expenses_size_filtering_year[
