cf95211fb7a046d43e8f53055f52540979f5e2f7
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-06-08 11:06:35 +0200

    Add: simple file to create the db

---
+++ sql/db_expense_tracker_creation.sql
@@ -0,0 +1,4 @@
+-- Creating the DB for the expense tracker.
+-- This should be done automatically in the Python script in case the DB
+-- does not exists. If it exists, than only the connection should happen.
+CREATE DATABASE db_expense_tracker;
