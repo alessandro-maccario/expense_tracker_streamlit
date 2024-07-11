25d12c3349176ae9edbe25c8f2054d275c9199de
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-07-11 23:07:21 +0200

    Remove: remove unuseful file

--- src/TEST_data_inserting.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from pkgs.sqlalchemy_db import TEST_Expense, engine
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-expense = TEST_Expense(expense_category="cat", expense_type="type", expense_price=3.01)
-session.add(expense)
-session.commit()
