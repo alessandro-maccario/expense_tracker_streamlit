4bdb9de5f69a887bf82bba79f9f8d1c7c67fc659
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-07-21 11:34:23 +0200

    Add: add DOCSTRING and test_filter_data() function

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,3 +1,30 @@
 """
 Script to test the metrics_dataclasses.py class and methods.
 """
+
+import unittest
+from src.pkgs.metrics_dataclasses import ExpenseMetric
+
+
+# create subclass that imports the unittest.TestCase class to derive its methods (inheritance)
+class TestMetric(unittest.TestCase):
+    """
+    Each methods in this class MUST be called starting with "test_*" so the script will know
+    which methods represents a test. Other methods that are not
+    called by starting with "test_*".
+    The TestCase class provides several assert methods to check for and report failures.
+    Check the following out for seeing which assert methods are available in the TestCase
+    class:
+        - https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug
+
+
+    Methods
+    -------
+
+    test_filter_data()
+        Test the filter_data function available in the metrics_dataclasses.py file.
+
+    """
+
+    def test_filter_data(self):
+        pass
