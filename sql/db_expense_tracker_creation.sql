c8c4af9c9515efc31bd6dd7bc00d97e55388fb50
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-07-28 17:22:13 +0200

    Remove: remove sql folder

--- sql/db_expense_tracker_creation.sql
+++
@@ -1,4 +0,0 @@
--- Creating the DB for the expense tracker.
--- This should be done automatically in the Python script in case the DB
--- does not exists. If it exists, than only the connection should happen.
-CREATE DATABASE db_expense_tracker;
