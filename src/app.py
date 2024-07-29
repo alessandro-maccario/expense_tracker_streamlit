78655c7c6dba935ccd210831e727042a403ac13a
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-07-29 12:19:51 +0200

    Test: adding the url for the data sample for cloud

--- src/app.py
+++ src/app.py
@@ -61,7 +61,10 @@ with st.sidebar:
             )
 
     # adding a download button to download sample of the data in a csv file
-    data_example_df = pd.read_csv("../static/data_example.csv", sep=";")
+    data_example_df = pd.read_csv(
+        "https://https://github.com/alessandro-maccario/expense_tracker_streamlit/blob/main/data/data_example.csv",
+        sep=";",
+    )
     # convert the dataframe to be sent to the donwload button
     data_example_encoded = convert_df(data_example_df)
 
