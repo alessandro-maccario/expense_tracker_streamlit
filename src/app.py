7f69f540ee546e5205385b8448da09e78ece83fa
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-07-29 14:34:17 +0200

    Fix: fix css rendering on the cloud

--- src/app.py
+++ src/app.py
@@ -389,38 +389,10 @@ if uploaded_file is not None:
 
         plot_waterfall.plot_waterfall_per_month(
             df_expenses, year_selection_waterfall, monthly_waterfall
-        )  # , annotation=annotation
+        )
 
     # --- CSS hacks --- #
-    css = """
-            /* use this code inside your css file */
-            div.st-emotion-cache-1xarl3l.e1i5pmia1{
-                background-color: #f5f5f5;
-                border: 2px solid;
-                padding: 10px 10px 10px 10px;
-                border-radius: 10px;
-                color: #ffc300;
-                box-shadow: 10px;
-                text-align: center;
-            }
-
-            div.st-emotion-cache-1xarl3l.e1i5pmia1{
-            background-color: #f5f5f5;
-            border: 1px solid; /* #900c3f */
-            border-radius: 50px;
-            padding: 10px 10px 10px 10px;
-            color: rgb(0, 68, 255);
-            }
-
-            /* Bold text */
-            div.st-emotion-cache-1xarl3l.e1i5pmia1{
-            font-weight: 900;
-            }
-            """
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
-    # with open("style/style.css") as f:
-    #     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
 
 else:
     st.text("To start the dashboard, please, upload a file using the button on the sidebar.")
